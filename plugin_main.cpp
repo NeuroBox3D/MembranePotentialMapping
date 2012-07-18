eae0a03043a07feaf356750e0ed7279204baa3c2
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2012-07-18 10:19:46 +0000

    A model for calcium permeability through the N-Methyl-D-Aspartat receptor (NMDAR) channel in hippocampal neurons in culture and additional unit tests for verification. Minor changes, i. e. annotations.

--- plugin_main.cpp
+++ plugin_main.cpp
@@ -31,11 +31,11 @@ InitUGPlugin_MembranePotentialMappingPlugin(ug::bridge::Registry* reg, string pa
    typedef membrane_potential_mapping::Vm2uG<std::string> TVm2uG;
 
    reg->add_class_<TVm2uG>("MembranePotentialMapper", grp)
-       .add_constructor<void (*)(std::string, std::string, bool)>("Timestep-File#File-Extension#NodesCanChange")
-       .add_method("get_potential", &TVm2uG::get_potential, grp)
+       .add_constructor<void (*)(std::string, std::string, bool)>("Initial timestep|load-dialog|endings=[\"csv\", \"txt\"];description=\"Timestep files\"#Suffix|selection|value=[\"csv\", \"txt\"]#StaticNodes")
+       .add_method("get_potential", &TVm2uG::get_potential, "Potential", "x#y#z#Timestep")
        .add_method("build_tree", &TVm2uG::buildTree, grp)
-       .add_method("get_potential_lin", &TVm2uG::get_potential_lin, grp)
-       .add_method("get_potential_bilin", &TVm2uG::get_potential_bilin, grp);
+       .add_method("get_potential_lin", &TVm2uG::get_potential_lin, "Potential", "#x#y#z#Timestep#k")
+       .add_method("get_potential_bilin", &TVm2uG::get_potential_bilin, "Potential", "#x#y#z#Timestep#k");
 
    // register functionalities of the Borg Graham model
    typedef membrane_potential_mapping::bg::BG TBG;
