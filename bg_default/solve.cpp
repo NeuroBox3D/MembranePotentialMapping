11e8f5cb23051494a6d3929764291b1c92ec393b
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2012-09-06 08:50:08 +0000

    Added new, corrected derivatives for Borg Graham channels CFP model case.

--- bg_default/solve.cpp
+++ bg_default/solve.cpp
@@ -30,10 +30,14 @@ using namespace ug::membrane_potential_mapping::bg;
 
 double solve_gating::I( double x, double y, double t, double myVm)
 {
-    return - g * pow( x, xp ) * pow( y, yp ) * ( BG::Voltage(myVm) - V_ret );
+    d_Ca = 0.0; // since no calcium is considered!
+    return - g * pow( x, xp ) * pow( y, yp ) * ( myVm - V_ret );
 }
 
 double solve_gating::I(double x, double y, double t, double myVm, double Ca_i, double Ca_o) {
+	d_CadCa_o = -(BG::BG::F^2*myVm*pow(x,xp)*pow(y,yp)*permeability*pow(valency,2.0))/(BG::R*BG::T*(std::exp((BG::F*myVm*valency)/(BG::R*BG::T)) - 1));
+	d_CadCa_i = -(BG::F^2*myVm*pow(x,xp)*pow(y,yp)*permeability*pow(valency,2.0))/(BG::R*BG::T*(1/std::exp((BG::F*myVm*valency)/(BG::R*BG::T)) - 1));
+
 	return pow( x, xp) * pow(y, yp) * permeability * valency*valency * BG::F*BG::F * myVm / BG::R / BG::T * ( Ca_i - Ca_o * std::exp( -valency * BG::F * myVm / (BG::R*BG::T) ) ) / ( 1 - std::exp( - valency * BG::F * myVm / (BG::R*BG::T) ) ) * 1e3;
 }
 
@@ -70,6 +74,8 @@ double solve_gating::calculate_current_expli_next_timestep( double time, double 
 
    current_x_0 = gate_x.x_1( time, current_x_0, delta_t, myVm);
    current_y_0 = gate_y.x_1( time, current_y_0, delta_t, myVm );
+
+
 
    return I( current_x_0, current_y_0, time, myVm, Ca_i, Ca_o );
 }
