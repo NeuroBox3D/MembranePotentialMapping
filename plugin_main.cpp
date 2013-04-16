ea8bd24cf06cb9d1abd02d987a52cec26034778a
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2013-04-16 10:01:15 +0000

    Fix for PM plugin (Thx Torbjörn).

--- plugin_main.cpp
+++ plugin_main.cpp
@@ -2,27 +2,12 @@
  * \file plugins/experimental/membrane_potential_mapping/plugin_main.cpp
  * \brief registry of the ug plugin membrane_potential_mapping
  *
- *  \date created on Apr 30, 2012
- *  \author stephan grein
+ * \date created on Apr 30, 2012
+ * \author stephan grein
  */
 
-/* std includes */
 #include <string>
 
-/* membrane potential mapping includes */
-#include "vm2ug.h"
-#ifdef MPMDEFAULT
-#include "bg_default/bg.h"
-#else
-#include "bg_simple/bg.h"
-#endif
-#include "transform.h"
-
-/* auxiliary function includes */
-#include "aux/edge_utilities.h"
-#include "aux/aux_bridge.cpp"
-
-/* ug includes */
 #include <bridge/util.h>
 #include <bridge/util_domain_dependent.h>
 #include <common/error.h>
@@ -33,31 +18,46 @@
 #include <lib_disc/domain.h>
 #include <lib_grid/lib_grid.h>
 
-/* begin namespace ug */
+#include "vm2ug.h"
+#ifdef MPMDEFAULT
+#include "bg_default/bg.h"
+#else
+#include "bg_simple/bg.h"
+#endif
+#include "transform.h"
+
+#include "aux/edge_utilities.h"
+#include "aux/aux_bridge.cpp"
+
+// begin namespace ug
 namespace ug {
-	/* begin namespace mpm */
+	// begin namespace mpm
 	namespace membrane_potential_mapping {
-
-		/* the functionality which is to be registered */
+		/*!
+		 * \defgroup mpm_plugin MembranePotentialMapping plugin
+		 * \ingroup experimental_plugins
+		 * \{
+		 *
+		 */
+		// the functionality which is to be registered
 		struct Functionality {
-
 			template <typename TDomain> static void
-			Domain(ug::bridge::Registry& reg, std::string& parentGroup)
+			Domain(ug::bridge::Registry& reg, const std::string& parentGroup)
 			{
-			   /* group membership of the membrane_potential_mapping plugin */
+			   // group membership of the membrane_potential_mapping plugin
 			   std::string grp(parentGroup);
 			   grp.append("Neuro/");
 
-			   /* typedefs */
+			   // typedefs
 			   typedef membrane_potential_mapping::Vm2uG<std::string> TVm2uG;
 			   typedef membrane_potential_mapping::bg::BG TBG;
 			   typedef membrane_potential_mapping::Transform TTransform;
 
-			   /* registry of auxiliarities (\see aux/edge_utilities.h) */
+			   // registry of auxiliarities (\see aux/edge_utilities.h)
 			   reg.add_function("get_edge_sum", static_cast<number (*)(TDomain&,  ISubsetHandler&, int, int)> (&membrane_potential_mapping::aux::EdgeSum<TDomain>), "", grp);
 			   reg.add_function("get_edge_sum_sq", static_cast<number (*)(TDomain&, ISubsetHandler&, int, int)> (&membrane_potential_mapping::aux::EdgeSumSq<TDomain>), "", grp);
 
-			   /* registry of Vm2uG (\see vm2ug.h) */
+			   // registry of Vm2uG (\see vm2ug.h)
 			   reg.add_class_<TVm2uG>("MembranePotentialMapper", grp)
 				  .add_constructor<void (*)(std::string, std::string, bool)>("Initial timestep|load-dialog|endings=[\"csv\", \"txt\"];description=\"Timestep files\"#Suffix|selection|value=[\"csv\", \"txt\"]#Static Nodes|selection|value=[True, False]")
 				  .add_method("get_potential", &TVm2uG::get_potential, "Potential|default", "x|default#y|default#z|default#Timestep|default")
@@ -65,7 +65,7 @@ namespace ug {
 				  .add_method("get_potential_lin", &TVm2uG::get_potential_lin, "Potential|default", "x|default#y|default#z|default#Timestep|default#k|default")
 				  .add_method("get_potential_bilin", &TVm2uG::get_potential_bilin, "Potential|default", "x|default#y|default#z|default#Timestep|default#k|default");
 
-			   /* registry of BG (\see bg.h) */
+			   // registry of BG (\see bg.h)
 			   reg.add_class_<TBG>("BorgGraham", grp)
 				  .add_constructor()
 				  .add_method("install_can_gates", &TBG::install_can_gates, grp)
@@ -88,7 +88,7 @@ namespace ug {
 				  #endif
 				  ;
 
-			   /* registry of Transform (\see transform.h) */
+			   // registry of Transform (\see transform.h)
 				reg.add_class_<TTransform>("TransformHocToObj", grp)
 				   .add_constructor<void (*)(std::string, std::string)>("hocfile|load-dialog|endings=[\"hoc\"];description=\"Hoc file for transformation\"#timestep directory|load-dialog;description=\"Location to store extracted timesteps\"#Delta t|default#steps|default#initial membrane potential|default")
 				   .add_method("get_hocfile", &TTransform::get_hocfile, "hocfile|default", "", grp)
@@ -98,11 +98,13 @@ namespace ug {
 				   .add_method("get_dt", &TTransform::get_dt, "Delta t", "", grp)
 				   .add_method("extract_timesteps_and_obj", (void (TTransform::*)(bool, std::string, std::string)) &TTransform::extract_timesteps_and_obj, "", "generate object|default#neugen exe|load-dialog|endings=[\"jar\"];description=\"Neugen executable\"#neutria exe|load-dialog;description=\"Neutria executable\"", grp);
 			}
-		/* end of functionality which is to be exported */
+		// end of functionality which is to be exported
 		};
-	/* end namespace mpm */
+	/// \}
+	// end namespace mpm
 	}
 
+	/// \addtogroup mempotmap_plugin
 	extern "C" void
 	InitUGPlugin_MembranePotentialMapping(bridge::Registry& reg, std::string& parentGroup)
 	{
@@ -125,5 +127,5 @@ namespace ug {
 			#warning "build with -DDIM=1, 2, or 3 for usage of plugin MembranePotentialMapping"
 		#endif
 	}
-/* end namespace ug */
+// end namespace ug
 }
