cec1c925ae82577b2d57dfd37aa7353e23fd8b18
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2015-04-28 14:30:25 +0000

    Fix for debug ids.

--- kdtree/kd_tree.cpp
+++ kdtree/kd_tree.cpp
@@ -1,16 +1,23 @@
-/*
- * kd_tree.cpp
+/*!
+ * \file kd_tree.cpp
  *
  *  Created on: Apr 28, 2015
  *      Author: stephan
  */
 
+/// includes
+#include <common/debug_id.h>
 
+/* \defgroup mpm_plugin Membrane Potential Mapping plugin
+ * \ingroup plugins_experimental
+ * \{
+ */
 namespace ug {
 	namespace membrane_potential_mapping {
 		////////////////////////////////////////////////////////////////////////
 		/// debug ids
 		////////////////////////////////////////////////////////////////////////
 		ug::DebugID MPM_KDTREE("MPM_DID.kdtree");
-	}
-}
+	} // namespace membrane_potential_mapping
+} // namespace ug
+//<! \}
